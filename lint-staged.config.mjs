7e64eb4292331049efd29b59091d1322e2fba398
mornya <mornya78@gmail.com>
2025-07-25 14:22:48 +0900

    lint-staged 설정 변경 / nodemon 추가

---
+++ lint-staged.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('lint-staged').Config} */
+export default {
+  '*.{css,scss}': (stagedFiles) => {
+    const files = stagedFiles.join(' ');
+    return [`prettier --write --ignore-path .prettierignore ${files}`];
+  },
+  '*.{ts,tsx,vue}': (stagedFiles) => {
+    const files = stagedFiles.join(' ');
+    return [
+      `prettier --write --ignore-path .prettierignore ${files}`,
+      'vue-tsc --noEmit --project tsconfig.json', // No add files
+      `eslint --no-cache ${files}`,
+    ];
+  },
+};
