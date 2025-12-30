41a80c8302179d1ba517a66918313e8e400865de
mornya <mornya78@gmail.com>
2025-12-30 21:45:03 +0900

    디펜던시 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { join } from 'path';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { defineConfig, type UserConfig } from 'vite';
-import vueDevTools from 'vite-plugin-vue-devtools';
+//import vueDevTools from 'vite-plugin-vue-devtools';
 
 const config: UserConfig = defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueJsx() /*, vueDevTools()*/],
   resolve: {
     alias: {
       '~': join(__dirname, './'),
